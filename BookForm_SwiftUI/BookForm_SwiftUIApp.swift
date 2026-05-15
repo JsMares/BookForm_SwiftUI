0a49984b960d3fbc8a4703fc4e8889a8c28fe1d3
Jonathan Soto <jsmares0105@gmail.com>
2026-05-14 23:21:45 -0600

    Initial Commit

---
+++ BookForm_SwiftUI/BookForm_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookForm_SwiftUIApp.swift
+//  BookForm_SwiftUI
+//
+//  Created by Jonathan Soto Mares on 14/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BookForm_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookForm_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookForm_SwiftUI
+//
+//  Created by Jonathan Soto Mares on 14/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
